48d9a2ea1c10bec7a9bf69f397898daec2b4101c
James Sleeman <james@gogo.co.nz>
2019-04-27 11:07:07 +1200

    Update documentation.

---
+++ docs/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,5 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "JQ6500_Serial.cpp", "_j_q6500___serial_8cpp.html", null ],
+    [ "JQ6500_Serial.h", "_j_q6500___serial_8h.html", "_j_q6500___serial_8h" ]
+];
--- docs/html/files.js
+++ docs/html/files.js
@@ -1,5 +1,4 @@
 var files =
 [
-    [ "JQ6500_Serial.cpp", "_j_q6500___serial_8cpp.html", null ],
-    [ "JQ6500_Serial.h", "_j_q6500___serial_8h.html", "_j_q6500___serial_8h" ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
