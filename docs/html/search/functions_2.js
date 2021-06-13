e33d2a91811386d76e9f33f13017dd1cacf7f2c5
Thijs Triemstra <info@collab.nl>
2021-06-13 15:26:09 +0200

    update api docs (using doxygen 1.9.1)

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['jq6500_5fserial',['JQ6500_Serial',['../class_j_q6500___serial.html#a9e998be9967b448b672ef90b9b689d76',1,'JQ6500_Serial']]]
+  ['jq6500_5fserial_45',['JQ6500_Serial',['../class_j_q6500___serial.html#a9e998be9967b448b672ef90b9b689d76',1,'JQ6500_Serial']]]
 ];
